6b99270227975a55d8c7f45b624b0925bb4addf7
jzProg <john_z94@hotmail.com>
2021-02-02 23:03:38 +0200

    test: Pricing microservice test added + improve CarController tests

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -2,15 +2,38 @@ package com.udacity.pricing;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.concurrent.ThreadLocalRandom;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void getPriceTest() throws Exception {
+		int randomInt =  ThreadLocalRandom.current().nextInt(1, 20);
+		mockMvc.perform(get("/services/price?vehicleId=" + randomInt))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(jsonPath("$.vehicleId", is(randomInt)))
+				.andExpect(jsonPath("$.price", any(Double.class)));
 	}
 
 }
