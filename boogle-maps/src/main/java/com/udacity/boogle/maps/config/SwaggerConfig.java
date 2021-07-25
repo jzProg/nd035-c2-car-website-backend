757e9bc571ba09f53dd325936528eb8871bdfcee
jzProg <john_z94@hotmail.com>
2021-07-25 23:16:01 +0300

    feat: added swagger support to all microservices

---
+++ boogle-maps/src/main/java/com/udacity/boogle/maps/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.udacity.boogle.maps.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Boogle Maps API",
+                "This API maintains address data",
+                "1.0",
+                "terms",
+                new Contact("jzProg", "https://github.com/jzProg", "john_z94@hotmail.com"),
+                "License of API", "license url", Collections.emptyList());
+    }
+}
